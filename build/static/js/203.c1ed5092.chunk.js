954a072a52559920f0db6dc722551c1c7b7dbf7f
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-29 00:41:16 +0200

    deploy 2

--- build/static/js/203.c1ed5092.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[203],{203:function(){0}}]);
-//# sourceMappingURL=203.c1ed5092.chunk.js.map
--- src/components/DetailedMovieData/DetailedMovieData.js
+++ src/components/DetailedMovieData/DetailedMovieData.js
@@ -1,13 +1,16 @@
 import Section from '../Section/Section'
 import s from './DetailedMovieData.module.css'
-import React, { lazy, Suspense } from 'react';
+// import React, { lazy, Suspense } from 'react';
 import { useParams, NavLink } from 'react-router-dom';
 import { ROUTERS } from '../../consts';
 import { Route } from 'react-router-dom';
-import Loader from '../Loader/Loader';
+// import Loader from '../Loader/Loader';
+import Cast from '../Cast/Cast'
+import Reviews from '../Reviews/Reviews'
 
-const Cast = lazy(() => import('../Cast/Cast'))
-const Reviews = lazy(() => import('../Reviews/Reviews'))
+
+// const Cast = lazy(() => import('../Cast/Cast'))
+// const Reviews = lazy(() => import('../Reviews/Reviews'))
 
 
 function DetailedMovieData() {
@@ -20,11 +23,11 @@ function DetailedMovieData() {
           <NavLink to={ROUTERS.MOVIES + `/${id}` + ROUTERS.CAST} exact activeClassName={s.selected} className={s.link}>Cast</NavLink>
           <NavLink to={ROUTERS.MOVIES + `/${id}` + ROUTERS.REVIEWS} exact activeClassName={s.selected} className={s.link}>Reviews</NavLink>
         </div>
-        <Suspense fallback={<Loader />}>
-          <Route path={ROUTERS.MOVIES + `/${id}` + ROUTERS.CAST} render={props => <Cast {...props} modieId={id} />} />
-          <Route path={ROUTERS.MOVIES + `/${id}` + ROUTERS.REVIEWS} render={props => <Reviews {...props} modieId={id} />} />
+        {/* <Suspense fallback={<Loader />}> */}
+        <Route path={ROUTERS.MOVIES + `/${id}` + ROUTERS.CAST} render={props => <Cast {...props} modieId={id} />} />
+        <Route path={ROUTERS.MOVIES + `/${id}` + ROUTERS.REVIEWS} render={props => <Reviews {...props} modieId={id} />} />
 
-        </Suspense>
+        {/* </Suspense> */}
       </div>
     </Section>
   )
