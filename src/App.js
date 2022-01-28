ef93712bcd500e0297cbefe41582dbdaaf4c7d22
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-28 03:04:10 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
-import Section from "./components/Section/Section";
-import React, { useState } from "react";
-import { Fragment } from "react/cjs/react.production.min";
-import propTypes from "prop-types";
-import Searchbar from "./components/Searchbar/Searchbar";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import Modal from "./components/Modal/Modal";
-import { ToastContainer } from "react-toastify";
+import { BrowserRouter, Route } from "react-router-dom"
+import { HomePage } from './pages/HomePage/HomePage'
 
-function App() {
-  const [searchName1, setSearchName] = useState('')
-  const [showModal, setShowModal] = useState(false)
-  const [option, setOption] = useState({})
-
-  const onSubmitSearchName = (inputData) => {
-    setSearchName(inputData)
-  }
-
-  const toggleModalWindow = (url, alt) => {
-    setShowModal(!showModal)
-    setOption({ imageUrl: url, imageAlt: alt })
-  };
-
+export const App = () => {
   return (
-    <Fragment>
-      <Searchbar onSubmitSearchName={onSubmitSearchName} />
-      <Section>
-        <ImageGallery searchName={searchName1} onClickLargeImageURL={toggleModalWindow} />
-      </Section>
-      {
-        showModal && (
-          <Modal
-            url={option.imageUrl}
-            alt={option.imageAlt}
-            onCloseModal={toggleModalWindow}
-          />
-        )
-      }
-      <ToastContainer autoClose={4000} />
-    </Fragment >
+    <BrowserRouter>
+      <header>
+        <h1>This is app</h1>
+
+      </header>
+      <ul>
+        <li>Home</li>
+        <li>Movies</li>
+      </ul>
+      <Route path='/' component={HomePage} exact />
+
+    </BrowserRouter
+    >
   )
 }
-
-App.propTypes = {
-  searchName: propTypes.string,
-}
-
-export default App
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,44 @@
+import s from './HomePage.module.css'
+
+import Section from "../../components/Section/Section";
+import React, { useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+import propTypes from "prop-types";
+import Searchbar from "../../components/Searchbar/Searchbar";
+import ImageGallery from "../../components/ImageGallery/ImageGallery";
+import Modal from "../../components/Modal/Modal";
+import { ToastContainer } from "react-toastify";
+
+export function HomePage() {
+  const [searchName1, setSearchName] = useState('')
+  const [showModal, setShowModal] = useState(false)
+  const [option, setOption] = useState({})
+
+  const onSubmitSearchName = (inputData) => {
+    setSearchName(inputData)
+  }
+
+  const toggleModalWindow = (url, alt) => {
+    setShowModal(!showModal)
+    setOption({ imageUrl: url, imageAlt: alt })
+  };
+
+  return (
+    <Fragment>
+      <Searchbar onSubmitSearchName={onSubmitSearchName} />
+      <Section>
+        <ImageGallery searchName={searchName1} onClickLargeImageURL={toggleModalWindow} />
+      </Section>
+      {
+        showModal && (
+          <Modal
+            url={option.imageUrl}
+            alt={option.imageAlt}
+            onCloseModal={toggleModalWindow}
+          />
+        )
+      }
+      <ToastContainer autoClose={4000} />
+    </Fragment >
+  )
+}
